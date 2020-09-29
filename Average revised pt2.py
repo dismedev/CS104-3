4f2f6ceeac9c4768477cdc6238207df8c1a865c9
dismedev <71734024+dismedev@users.noreply.github.com>
2020-09-29 17:53:49 -0400

    version 2

---
+++ Average revised pt2.py
@@ -0,0 +1,14 @@
+numberofscores = 0
+score = 0
+total = 0
+average = 0.0
+scorecount = 0
+numberofscores = int(input("Please enter the number of scores you want to average: "))
+#What python loop structure could we use to repeat the next 3 lines
+score = int(input("please enter a score: "))
+total = total + score
+scoreCount = scoreCount + 1
+
+scoreCount = numberofScores
+average = total / numberofScores
+print(average)
